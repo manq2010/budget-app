4023673b43afbdc1892784ce78ff540cb651ce87
manq2010 <manq2010@gmail.com>
2023-03-23 19:25:49 +0200

    Add routing unit tests

--- spec/routing/expenses_routing_spec.rb
+++ spec/routing/expenses_routing_spec.rb
@@ -3,36 +3,36 @@ require "rails_helper"
 RSpec.describe ExpensesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/expenses").to route_to("expenses#index")
+      expect(get: "/categories/1/expenses").to route_to("expenses#index", group_id: "1")
     end
 
     it "routes to #new" do
-      expect(get: "/expenses/new").to route_to("expenses#new")
+      expect(get: "categories/1/expenses/new").to route_to("expenses#new", group_id: "1")
     end
 
     it "routes to #show" do
-      expect(get: "/expenses/1").to route_to("expenses#show", id: "1")
+      expect(get: "/categories/1/expenses/1").to route_to("expenses#show", id: "1", group_id: "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/expenses/1/edit").to route_to("expenses#edit", id: "1")
+      expect(get: "/categories/1/expenses/1/edit").to route_to("expenses#edit", id: "1", group_id: "1")
     end
 
 
     it "routes to #create" do
-      expect(post: "/expenses").to route_to("expenses#create")
+      expect(post: "/categories/1/expenses").to route_to("expenses#create", group_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/expenses/1").to route_to("expenses#update", id: "1")
+      expect(put: "/categories/1/expenses/1").to route_to("expenses#update", id: "1", group_id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/expenses/1").to route_to("expenses#update", id: "1")
+      expect(patch: "/categories/1/expenses/1").to route_to("expenses#update", id: "1", group_id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/expenses/1").to route_to("expenses#destroy", id: "1")
+      expect(delete: "/categories/1/expenses/1").to route_to("expenses#destroy", id: "1", group_id: "1")
     end
   end
 end
