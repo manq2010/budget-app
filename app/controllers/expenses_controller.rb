2a25dbc4d618244aaa02264c2816b701e6770c8c
manq2010 <manq2010@gmail.com>
2023-03-24 16:16:38 +0200

    Add third batch of changes

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,11 +2,6 @@ class ExpensesController < ApplicationController
   before_action :set_group, only: %i[new create edit update destroy]
   before_action :set_expense, only: %i[edit update destroy]
 
-  # def index
-  #   @group = Group.find(params[:group_id])
-  #   @expenses = @group.expenses.where(author_id: current_user.id)
-  # end
-
   def index
     @expenses = Expense.all
   end
@@ -16,31 +11,24 @@ class ExpensesController < ApplicationController
     @group = Group.find(params[:group_id])
   end
 
-  # GET /expenses/new
   def new
     @group = Group.find(params[:group_id])
     @expense = Expense.new
   end
 
-  # GET /expenses/1/edit
   def edit
     @group = Group.find(params[:group_id])
   end
 
-  # POST /expenses
-  # POST /expenses.json
   def create
-    @group = Group.find(params[:group_id])
-    @expense = current_user.expenses.new(expense_params)
+    # @expense = Expense.new(expense_params)
+    @expense = Expense.new(expense_params)
     @expense.author_id = current_user.id
-
-    @my_group_object = Group.find_by(id: params[:group_id])
-
-    @my_group_object.expenses << [@expense]
-
+    @expense.groups = Group.where(id: params[:expense][:group_ids]).includes([:user])
+    
     respond_to do |format|
       if @expense.save
-        format.html { redirect_to group_path(@group), notice: 'Expense was successfully created.' }
+        format.html { redirect_to group_path(id: params[:expense][:group_ids].last), notice: 'Expense was successfully created.' }
         format.json { render :show, status: :created, location: @expense }
       else
         format.html { render :new }
@@ -48,9 +36,7 @@ class ExpensesController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /expenses/1
-  # PATCH/PUT /expenses/1.json
+  
   def update
     respond_to do |format|
       if @expense.update(expense_params)
@@ -63,8 +49,6 @@ class ExpensesController < ApplicationController
     end
   end
 
-  # DELETE /expenses/1
-  # DELETE /expenses/1.json
   def destroy
     @expense.destroy
     respond_to do |format|
@@ -75,7 +59,6 @@ class ExpensesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_expense
     @expense = Expense.find(params[:id])
     @group = @expense.groups.first
@@ -85,8 +68,7 @@ class ExpensesController < ApplicationController
     @group = Group.find(params[:group_id])
   end
 
-  # Only allow a list of trusted parameters through.
   def expense_params
-    params.require(:expense).permit(:name, :amount, :author_id)
-  end
+    params.require(:expense).permit(:name, :amount, group_ids: [])
+  end  
 end
